b954eb4e80b68df9f95fde8e38dd23b3746891d7
JEREMY BON <jeremy_bon@carrefour.com>
2024-03-20 07:05:13 +0100

    ✨ remove old csv

--- EconoMe/__main__.py
+++ EconoMe/__main__.py
@@ -1,5 +1,10 @@
 from scraping.lcl import convert_LCLpdf_to_df
+import os
 
 if __name__ == "__main__":
     df = convert_LCLpdf_to_df("EconoMe/LCL")
-    df.to_csv("LCL.csv", index=False)
+    filename = "LCL.csv"
+    if os.path.exists(filename):
+    # Supprimer le fichier CSV existant
+        os.remove(filename)
+    df.to_csv(filename, index=False)
